a26f6762ac351bbeb62e6099d48d4f756b780d88
LEGENDRE <legendre.gui@gmail.com>
2015-01-16 11:06:54 +0100

    Modification  16 Janvier

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'home/contact'
   get 'home/faq'
   get 'contact/send_email'
+  get 'dashboard' => "dashboard#show"
+
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
