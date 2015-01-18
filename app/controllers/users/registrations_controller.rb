056f5a8a08af0f460bf8b854a5c7cb8e7877b805
LEGENDRE <legendre.gui@gmail.com>
2015-01-18 19:40:50 +0100

    add error message devise and french devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,4 +12,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def edit
     render :edit, layout: "backoffice"
   end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :phone)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :phone)
+  end
 end
