b044f7d6e380cb43c4162740f9e824a4859fb9be
LEGENDRE <legendre.gui@gmail.com>
2015-01-29 15:00:21 +0100

    less puts

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,5 @@
 class ContactMailer < ActionMailer::Base
   def send_to_contact(contact, email, user)
-    puts email.inspect
     mail subject: email[:subject],
          to: contact.email,
          from: user.email,
