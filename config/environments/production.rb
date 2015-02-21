519af88fc3c882f00862c772d53bb3e537055941
LEGENDRE <legendre.gui@gmail.com>
2015-02-21 15:30:52 +0100

    fix assets pipeline 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
+  config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
 
   # Generate digests for assets URLs.
   config.assets.digest = true
