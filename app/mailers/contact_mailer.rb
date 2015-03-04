6ab81a0811980b4efe9369b8ffeb89ce6e3917f7
LEGENDRE <legendre.gui@gmail.com>
2015-03-04 17:06:03 +0100

    fix mail add reply to

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,8 +2,9 @@ class ContactMailer < ActionMailer::Base
   def send_to_contact(contact, email, user)
     mail subject: email[:subject],
          to: contact.email,
-         from: user.email,
+         from: "no-reply@maboutique.pro",
          body: email[:body],
-         content_type: "text/html"
+         content_type: "text/html",
+         reply_to: user.email
   end
 end
